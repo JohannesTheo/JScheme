bfb92f332a5daff83b95c2fc153568f67e6cd6c3
Jojo <jt031@hdm-stuttgart.de>
2016-06-12 19:10:49 +0200

    ADD skip "whitespaces and control chars" support UPDATE readNumber #comment: sum up the input number UPDATE integer print

--- print.c
+++ print.c
@@ -7,7 +7,7 @@ js_print(OBJ o){
 	enum tag kindOf = o->u.integer.tag;
 
 	if( kindOf == T_INTEGER ) {
-		printf("%c" , (int) o->u.integer.intVal);
+		printf("%ld" , o->u.integer.intVal);
 	} else {
 		printf("#<unimpl. print>");
 	}
--- reader.c
+++ reader.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "jscheme.h"
 
+int
+isWhiteSpace(char ch){
+	
+	return( (ch == ' ') || (ch == '\n') || (ch == '\t') || (ch == '\r'));
+
+}
+
+char
+skipWhiteSpace(){
+
+	char ch;
+       
+	do{
+		ch = fgetc(stdin);
+	} while (isWhiteSpace(ch));
+
+	return ch;
+}
 
 int
 isDigit(char ch) {
@@ -11,27 +30,31 @@ isDigit(char ch) {
 OBJ
 js_read(){
 
-	OBJ input = NULL;
+	char ch = skipWhiteSpace();
 	
-	char ch = fgetc(stdin);
-
-	while( !isDigit(ch) ){
-		if( (ch != '\n') && (ch !=' ')){
-			printf("<unimpl. read> %c\n", ch);
-		}
-		ch = fgetc(stdin);
+	if(isDigit(ch)){
+		return readNumber(ch);
 	}
-
-	input = readNumber(ch);
-	return input;
+	printf("#<unimpl. read> %c\n", ch);
+	exit(-1);
 }
 
 
 OBJ
-readNumber(char ch){
+readNumber(char firstChar){
 
-	OBJ retVal = newInteger(ch);
+	jscheme_int64 iVal = 0;
+	char ch;
+	OBJ retVal;
 
+	// substract the ASCII value of '0' from char to get the actual value between 0 and 9. 
+	iVal = (int)firstChar - '0';
+
+	while( isDigit( ch = fgetc(stdin))){
+		iVal = iVal * 10 + ( (int)ch - '0');
+	}
+	
+	retVal = newInteger( iVal );
 	return retVal;
 }
 
