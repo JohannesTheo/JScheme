4d4d70fdea42cded5cb847a4e43eb60ec6a10eb5
Jojo <jt031@hdm-stuttgart.de>
2016-08-03 11:58:00 +0200

    ADD overflow check to builtin_plus ADD builtin_minus implementation with overflow check

--- builtinFunctions.c
+++ builtinFunctions.c
@@ -3,7 +3,9 @@
 OBJ
 builtin_plus(int numArgs){
 
-	jscheme_int64 sum = 0;
+	jscheme_int64  start = 0;
+	jscheme_int64 *sum = NULL;
+	sum = &start;
 	int i;
 
 	for(i = 0; i < numArgs; i++){
@@ -12,21 +14,59 @@ builtin_plus(int numArgs){
 		if( !ISINTEGER(theArg)){
 			js_error("(+): non-integer argument", theArg);
 		}
-		// TO-DO prevent OVERFLOW
-		sum = sum + INTVAL(theArg);
+		
+	       	if(__builtin_saddl_overflow( *sum, INTVAL(theArg), sum)){
+			// clean evalStack
+			POPN((numArgs - 1) - i);
+			printf("spIndex: %d\n", spIndex);
+			js_error("(+): integer overflow", newInteger(*sum));	
+		};
 	}
-	return newInteger(sum);
+	return newInteger(*sum);
 }
 
-
-// TO-DO: implement builtins 
 OBJ
 builtin_minus(int numArgs){
-	if(numArgs == 0){
-		js_error("(-): at least one arg expected", js_nil);
-	}
+	OBJ theArg;
+	
+	switch (numArgs){
+		case 0:
+			js_error("(-): at least one arg expected", js_nil);
+			/* NOT REACHED */
+		case 1:
+			theArg = POP();
+			if( !ISINTEGER(theArg)){
+				js_error("(-): non-integer argument", theArg);
+			}
+			return newInteger( -INTVAL(theArg) );
+		default:
+			theArg = NTH_ARG(numArgs, 0);
 
-	error("unimpl. minus", __FILE__, __LINE__);
+			if( !ISINTEGER(theArg)){
+				js_error("(-): non-integer argument", theArg);
+			}
+			
+			jscheme_int64 *difference = NULL;
+			jscheme_int64 start = INTVAL(theArg);
+			difference = &start;
+
+			for(int i = 1; i < numArgs; i++){
+				OBJ nextArg = NTH_ARG(numArgs, i);
+				if( !ISINTEGER(nextArg)){
+					js_error("(-): non-integer argument", theArg);
+				}
+				if(__builtin_ssubl_overflow(*difference, INTVAL(nextArg), difference)){
+					// clean stack
+					POPN(numArgs);
+					printf("spIndex: %d\n", spIndex);
+					js_error("(-): integer overflow", newInteger(*difference));
+				};
+			}
+			POPN(numArgs);
+			printf("spIndex: %d\n", spIndex);
+			return newInteger(*difference);
+	}	
+	/* NOT REACHED */
 	return js_nil;
 }
 
